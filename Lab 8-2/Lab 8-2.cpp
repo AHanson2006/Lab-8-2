7f8b2283409795a18b8f968158c7c2e7686c3797
AHanson2006 <a-hanson.3@onu.edu>
2025-10-27 19:37:37 -0400

    User input added

--- Lab 8-2/Lab 8-2.cpp
+++ Lab 8-2/Lab 8-2.cpp
@@ -5,25 +5,33 @@ using namespace std;
 int countWords(string str);
 
 int main() {
-	string str = "Hello World";
-	string str2 = "Hello              World";
+	string str;
+	do {
+		cout << "Enter Text: ";
+		getline(cin, str);
 
-	cout << "There are " << countWords(str) << " word(s) in string 1, and " << countWords(str2) << " word(s) in string 2.";
+		if (str != "Q" && str != "q") {
+			cout << "There are " << countWords(str) << " word(s)" << endl;
+		}
+		
+	} while (str != "Q" && str != "q");
 }
 int countWords(string str) {
 	int wordCount = 1;
 	int spaceCount = 0;
 
 	for (int i = 0; i < size(str); i++) {
-		if (str[i] == ' ') {
+		if (str[i] == ' ' && spaceCount == 0) {
 			spaceCount = spaceCount + 1;
-			if (spaceCount == 1) {
-				wordCount = wordCount + 1;
+			wordCount = wordCount + 1;
+			if (spaceCount > 1) {
+				wordCount = wordCount - 1;
 
 			}
-			else if (str[i] != ' ') {
-				spaceCount = 0;
-			}
+			
+		}
+		else if (str[i] != ' ') {
+			spaceCount = 0;
 		}
 		
 	}
